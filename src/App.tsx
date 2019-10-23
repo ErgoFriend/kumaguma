f3ee5ddb70fbacd8d7e960283a823cf83423190f
ErgoFriend <erg.friend@gmail.com>
2019-10-23 21:45:59 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import Styled from 'styled-components';
+
+const App: React.FC = () => {
+  return (
+    <AppStyle>
+      <header className="App-header">
+        <LogoStyle src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </AppStyle>
+  );
+}
+
+export default App;
+
+const AppStyle = Styled.div`
+  text-align: center;
+`;
+
+const HeaderStyle = Styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const LogoStyle = Styled.img`
+  height: 40vmin;
+`;
+
+const LinkStyle = Styled.a`
+  color: #09d3ac;
+`;
